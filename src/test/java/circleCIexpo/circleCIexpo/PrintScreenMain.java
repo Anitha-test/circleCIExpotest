2031b9af0bded9230bde8f54f75c3f6390093a58
Anitha-test <58299449+Anitha-test@users.noreply.github.com>
2023-03-13 14:28:59 +0530

    Update PrintScreenMain.java

--- src/test/java/circleCIexpo/circleCIexpo/PrintScreenMain.java
+++ src/test/java/circleCIexpo/circleCIexpo/PrintScreenMain.java
@@ -21,7 +21,12 @@ public class PrintScreenMain {
 	     System.out.println("Main Method Print Statement 123");
 	     System.out.println("Changes made for only crazy erperiment branch");
 	     String env = System.getenv("DUMMY");
-		 System.out.println("Print the environment variable "+env);
+	     ChromeOptions chromeOptions = new ChromeOptions();
+	    // chromeOptions.addArguments("--remote-allow-origins=*");
+	     ChromeDriver driver=new ChromeDriver(chromeOptions);
+	     driver.get("http://google.com");
+	     System.out.println("Print the environment variable "+env);
+	     System.out.println("Print the environment variable "+env);
 	    }
 	    @AfterMethod
 	    public void afterMethod() {
